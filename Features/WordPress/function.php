ccf2f22ca58e0b483502cef4f092f29bcba74e91
Igor Khmelevskiy <parkour22@bigmir.net>
2019-05-10 21:44:18 +0300

    wp_function

---
+++ Features/WordPress/function.php
@@ -0,0 +1,16 @@
+<?php
+//Хуки
+//При помощи этого хука можно вывести надпись в футере только на одной странице, или на всех
+add_action('wp_footer', 'art_wp_footer' );
+function art_wp_footer() {
+    if (is_single( )) {
+        echo 'this is hook';
+    }
+}
+//функция отключения хука
+remove_action('Какой экшн', 'какую функцию', 'указать приоретен например 30');
+//фильтры
+add_filter('the_content', 'art_before_text' );
+function art_before_text($content) {
+    return 'текст выводится перед основным контентом' . $content;
+}
