81912196bc3a3298d744208d829a775b5a186b74
Igor Khmelevskiy <parkour22@bigmir.net>
2019-05-25 17:03:55 +0300

    pregress line

---
+++ Features/Progress line/main.js
@@ -0,0 +1,12 @@
+const progress = document.querySelector('.progress');
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e) {
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let per = windowScroll / windowHeight * 100;
+    // console.log(per);
+
+    progress.style.width = per + '%';
+}
